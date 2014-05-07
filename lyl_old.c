e15ec992b5ed30726e54c79f86dd28d5459ef4a6
lylnanjing <279369001@qq.com>
2014-05-07 17:18:22 +0800

    liyongling

---
+++ lyl_old.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lyl_old.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年05月07日 16时21分03秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+	char *p="Hello";
+	strcat(p,"World");
+	printf("p is %s.\n",p);
+}
